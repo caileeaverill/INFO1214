2827b45791b9e9a257cf3d5865bda0724ce1a241
Cailee <caileeaverill@gmail.com>
2025-09-18 17:26:07 -0400

    Initial commit

---
+++ src/week3/inClassAssignment/practiceA/AssignmentA.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package week3.inClassAssignment.practiceA;
+
+/**
+ * 
+ */
+public class AssignmentA {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/week3/inClassAssignment/practiceB/AssignmentB.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package week3.inClassAssignment.practiceB;
+
+/**
+ * 
+ */
+public class AssignmentB {
+
+}
